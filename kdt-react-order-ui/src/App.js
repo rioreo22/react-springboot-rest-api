806dbf4c90ddf59f9c3f0d1c00e9812156675379
Hyunji <rioreo2022@gmail.com>
2022-05-03 04:28:17 +0900

    주문관리API(3) 끝

--- kdt-react-order-ui/src/App.js
+++ kdt-react-order-ui/src/App.js
@@ -7,19 +7,43 @@ import axios from "axios";
 
 function App() {
     const [products, setProducts] = useState([
-        {id: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: 5000},
-        {id: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: 4000},
-        {id: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 3000},
-        {id: 'uuid-4', productName: '콜롬비아 커피4', category: '커피빈', price: 2000}
+        {productId: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: 5000},
+        {productId: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: 4000},
+        {productId: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 3000},
+        {productId: 'uuid-4', productName: '콜롬비아 커피4', category: '커피빈', price: 2000}
     ]);
 
     const [items, setItems] = useState([]);
-    const handleAddClicked = id => {
-        const product = products.find(v=> v.id===id);
-        const found = items.find(v => v.id === id);
-        const updatedItems = found ? items.map(v =>( v.id === id) ? { ...v, count: v.count + 1}:v):[...items, { ...product, count:1}]
+
+    const handleOrderSubmit = (order) => {
+        if (items.length === 0) {
+            alert("아이템을 추가해 주세요");
+        } else {
+            axios.post('http://localhost:8080/api/v1/orders', {
+                email: order.email,
+                address: order.address,
+                postcode: order.postcode,
+                orderItems:
+                    items.map(v => ({
+                        productId: v.productId,
+                        category: v.category,
+                        price: v.price,
+                        quantity: v.count
+                    }))
+            }).then(v => alert("주문이 정상적으로 접수 되었습니다."), e => {alert("장애"); console.log(e)});
+        }
+        console.log(order, items);
+    }
+
+    const handleAddClicked = productId => {
+        const product = products.find(v => v.productId === productId);
+        const found = items.find(v => v.productId === productId);
+        const updatedItems = found ? items.map(v => (v.productId === productId) ? {
+            ...v,
+            count: v.count + 1
+        } : v) : [...items, {...product, count: 1}]
         setItems(updatedItems);
-        console.log(products.find(v => v.id === id), "added!");
+        console.log(products.find(v => v.productId === productId), "added!");
     }
 
     useEffect(() => {
@@ -38,7 +62,7 @@ function App() {
                         <ProductList products={products} onAddClick={handleAddClicked}/>
                     </div>
                     <div className="col-md-4 summary p-4">
-                        <Summary items={items}/>
+                        <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
                     </div>
                 </div>
             </div>
